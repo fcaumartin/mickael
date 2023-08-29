885f354c083e411173335bf229446345bc8ba2a7
frc <fcaumartin@gmail.com>
2023-08-29 16:20:06 +0200

    add fixtures

--- symfony/src/DataFixtures/AppFixtures.php
+++ symfony/src/DataFixtures/AppFixtures.php
@@ -11,10 +11,16 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $c = new Client();
-        $c->setNom("toto");
-        $c->setPrenom("toto");
-        $c->setVille("toto");
+        $c->setNom("toto1");
+        $c->setPrenom("toto1");
+        $c->setVille("toto1");
         $manager->persist($c);
+
+        $c2 = new Client();
+        $c2->setNom("toto2");
+        $c2->setPrenom("toto2");
+        $c2->setVille("toto2");
+        $manager->persist($c2);
 
         $manager->flush();
     }
